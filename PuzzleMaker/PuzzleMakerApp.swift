a5726a13158564a1511e72c708a0366aaac6db2a
Shota <sanco0n@hotmail.com>
2026-01-17 21:46:44 +0900

    Initial Commit

---
+++ PuzzleMaker/PuzzleMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuzzleMakerApp.swift
+//  PuzzleMaker
+//
+//  Created by santamaru on 2026/01/17.
+//
+
+import SwiftUI
+
+@main
+struct PuzzleMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PuzzleMakerTests/PuzzleMakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PuzzleMakerTests.swift
+//  PuzzleMakerTests
+//
+//  Created by santamaru on 2026/01/17.
+//
+
+import Testing
+@testable import PuzzleMaker
+
+struct PuzzleMakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
